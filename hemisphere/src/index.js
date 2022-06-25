f41e47efd1c70737159f753d4817bbfe479b52f2
haticenur.4455 <66225075+HNK-hub@users.noreply.github.com>
2022-06-26 01:04:19 +0300

    update

--- hemisphere/src/index.js
+++ hemisphere/src/index.js
@@ -1,18 +1,31 @@
 import React from "react";
 import  ReactDOM  from "react-dom";
 
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        (possition) => console.log(possition),
-        (error) => console.log(error)
-    );
-    return (
-        <div>
-            Hello World
-        </div>
-    )
+class App extends React.Component {
+    constructor (props)
+    {
+        super(props)
+        this.state= {latitude : 25, errorMessage : ''} 
+            window.navigator.geolocation.getCurrentPosition(
+            (possition) => {
+                this.setState({latitude :possition.coords.latitude})
+            },
+            (error) => {
+                this.setState({errorMessage : error.message})
+            }
+        );
+    } 
+    render () {
+        if(this.state.errorMessage && !this.state.latitude){
+            return<div>{this.state.errorMessage}</div>
+        }
+        if(!this.state.errorMessage && this.state.latitude){
+            return<div>{this.state.latitude}</div>
+        }
+        else
+        return <div>Loading...</div>
+    }
 }
-
 ReactDOM.render(
     <App/>,
     document.querySelector('#root')
